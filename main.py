6e7de533b6d83598b9436fb163d3c84d0c854c95
Rez-programmer <rezacodm.reza8230@gmail.com>
2024-07-28 08:52:24 +0330

    Implemented the preprocessing step

--- main.py
+++ main.py
@@ -2,3 +2,20 @@ import cv2
 from sklearn.model_selection import train_test_split
 import glob
 
+data = []
+
+
+def preprocessing(add):
+    img = cv2.imread(add)
+    img = cv2.resize(img, (32, 32))
+    # Normalizing each pixel.
+    img = img/255.0
+    # Transforming the matrix into a feature vector(each pixel is a feature)
+    img = img.flatten()
+    return img
+
+
+for address in glob.glob("fire_dataset\\*\\*"):
+    image = preprocessing(address)
+    data.append(image)
+     
