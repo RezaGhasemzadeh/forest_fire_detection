0e401ddb556be003998e89404ec07c7870cdad0f
Rez-programmer <rezacodm.reza8230@gmail.com>
2024-07-28 09:28:24 +0330

    Splited data into test and train with some refactoring

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import cv2 
 import glob
 from sklearn.model_selection import train_test_split
-
-data = []
-labels = []
+import numpy as np
 
 
 def preprocessing(add):
@@ -17,8 +15,22 @@ def preprocessing(add):
     return img, label
 
 
-for address in glob.glob("fire_dataset\\*\\*"):
-    image, label = preprocessing(address)
-    data.append(image)
-    labels.append(label)
-     
+def load_data():
+    data = []
+    labels = []
+    for (i, address) in enumerate(glob.glob("fire_dataset\\*\\*")):
+        image, label = preprocessing(address)
+        data.append(image)
+        labels.append(label)
+        if i % 100 == 0:
+            print(f"{i}/998 processed")
+
+    # Because sklearn only accepts numpy arrays or pandas dataframes, we convert our list to 
+    # a numpy array but there is not need to convert labels to array
+    data = np.array(data)
+    train_features, test_features, train_labels, test_labels = train_test_split(data, labels, test_size=0.2)
+    return train_features, test_features, train_labels, test_labels
+
+
+train_features, test_features, train_labels, test_labels = load_data()
+
